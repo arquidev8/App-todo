1fea4aae543138375e498d0d9e69c779ca25133a
Hector <hectorcreatives08@gmail.com>
2023-12-10 22:10:42 -0400

    add readme

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # Vue 3 example with Pinia
 
 ## Usage
-
+npm install 
+npm run dev
 ```sh
 yarn
 yarn dev
---
+++ src/layouts/default.vue
@@ -0,0 +1,67 @@
+<template>
+  <header class="app-header">
+    <div class="logo">ToDo App</div>
+    
+  </header>
+
+  <hr />
+
+  <slot />
+
+  <hr />
+
+  <footer>Made with Hector Hernandez</footer>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss">
+.app-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #3498db;
+  color: #fff;
+  padding: 10px;
+}
+
+.logo {
+  font-size: 1.5rem;
+  font-weight: bold;
+}
+
+.nav-menu {
+  display: flex;
+}
+
+.nav-menu a {
+  text-decoration: none;
+  color: #fff;
+  margin: 0 10px;
+}
+
+header,
+footer {
+  text-align: center;
+  font-weight: bold;
+  background-color: #3498db;
+  color: #ecf0f1;
+  padding: 10px;
+}
+
+@media only screen and (max-width: 600px) {
+  .app-header {
+    flex-direction: column;
+    align-items: flex-start;
+  }
+
+  .nav-menu {
+    margin-top: 10px;
+  }
+
+  .nav-menu a {
+    display: block;
+    margin-bottom: 5px;
+  }
+}
+</style>
